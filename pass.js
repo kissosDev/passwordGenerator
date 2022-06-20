1391c756ae2f131f880766307806a5a9946b3044
kissosDev <kissosDev>
2022-06-21 01:56:13 +0300

    interate over passwordfeature

--- pass.js
+++ pass.js
@@ -1,30 +1,57 @@
 const log = console.log;
-console.log(`Liram Put Resk`);
+console.log(`Liram Put Resek`);
+
 function getRandomArbitrary(min, max) {
-    return Math.random() * (max - min) + min;
+  return Math.random() * (max - min) + min;
 }
 
 function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+  return Math.floor(Math.random() * max);
 }
 
 let asciPassPossibilities = [];
-let asciiRanges = [[33, 47], [48, 57], [65, 90], [97, 122]];
+let asciiRanges = [
+  [33, 47],
+  [48, 57],
+  [65, 90],
+  [97, 122],
+];
 function addPosibilitiesToArray() {
-    for (let rangeItem = 0; rangeItem < asciiRanges.length; rangeItem++) {
-        const element = asciiRanges[rangeItem];
-        for (let i = element[0]; i < element[1]; i++) {
-            asciPassPossibilities.push(i);
-        }
-    }
-}
+  let rangeItem = 0;
+  let element = asciiRanges[rangeItem];
+  for (let prop in passwordFeatures) {
+    console.log(asciPassPossibilities);
+    console.log(passwordFeatures[prop]);
+    console.log(prop);
 
-addPosibilitiesToArray();
+    if (passwordFeatures[prop]) {
+      for (
+        let i = asciiRanges[rangeItem][0];
+        i < asciiRanges[rangeItem][1];
+        i++
+      ) {
+        // console.log(element[0]);
+        console.log(i);
+        console.log(rangeItem);
+        asciPassPossibilities.push(i);
+      }
+      rangeItem++;
+      console.log(rangeItem);
+    } else {
+      rangeItem++;
+      continue;
+    }
+  }
+}
 
 function getPasswordChar() {
-    let passChar = String.fromCharCode(asciPassPossibilities[getRandomInt(asciPassPossibilities.length)]);
-    return passChar
+  let passChar = String.fromCharCode(
+    asciPassPossibilities[getRandomInt(asciPassPossibilities.length)]
+  );
+  return passChar;
 }
+
+let passwordLength = 20;
 
 // function getFullPassword() {
 //     for (let i = 0; i < passwordFeatures.length; i++) {
@@ -74,12 +101,28 @@ function getPasswordChar() {
 // }
 
 const passwordFeatures = {
-    length: 20,
-    includeLower: true,
-    includeUpper: true,
-    includeNumber: true,
-    includeSymbol: true,
+  // length: 20,
+  includeLower: true,
+  includeUpper: true,
+  includeNumber: true,
+  includeSymbol: true,
 };
+
+addPosibilitiesToArray();
+
+/*
+cost symbols = [];
+const numbers = [];
+const upperCase = [];
+const lowerCase = [];
+
+const initialPass = getMinimumRequirements();
+const possibleChars = getPossibleCharSubset();
+const rawPassword = initialPass + (length(password) - length(initial) number of random choices from possibleChars)
+const password = shuffle(rawPassword)
+
+
+*/
 
 // function generatePassword(passwordFeatures) {}
 
@@ -92,36 +135,35 @@ const symbolsEl = document.getElementById("symbols");
 const generateEl = document.getElementById("generate");
 
 function generatePasswordFeatures(passwordFeatures) {
-    passwordFeatures.length = lengthEl.value;
-    passwordFeatures.includeLower = lowercaseEl.checked;
-    passwordFeatures.includeUpper = uppercaseEl.checked;
-    passwordFeatures.includeNumber = numbersEl.checked;
-    passwordFeatures.includeSymbol = symbolsEl.checked;
+  passwordLength = lengthEl.value;
+  passwordFeatures.includeLower = lowercaseEl.checked;
+  passwordFeatures.includeUpper = uppercaseEl.checked;
+  passwordFeatures.includeNumber = numbersEl.checked;
+  passwordFeatures.includeSymbol = symbolsEl.checked;
 }
 
-
 lowercaseEl.addEventListener("click", function updateLowerCase(e) {
-    passwordFeatures.includeLower = lowercaseEl.checked;
+  passwordFeatures.includeLower = lowercaseEl.checked;
 });
 uppercaseEl.addEventListener("click", function updateUpperCase(e) {
-    passwordFeatures.includeUpper = uppercaseEl.checked;
+  passwordFeatures.includeUpper = uppercaseEl.checked;
 });
 numbersEl.addEventListener("click", function updateNumber(e) {
-    passwordFeatures.includeNumber = numbersEl.checked;
+  passwordFeatures.includeNumber = numbersEl.checked;
 });
 symbolsEl.addEventListener("click", function updateSymbol(e) {
-    passwordFeatures.includeSymbol = symbolsEl.checked;
+  passwordFeatures.includeSymbol = symbolsEl.checked;
 });
 
 lengthEl.addEventListener("change", function updateLength(e) {
-    passwordFeatures.length = lengthEl.value;
+  passwordLength = lengthEl.value;
 });
 
 generateEl.addEventListener("click", function getFullPassword(e) {
-    let password = ""
-    for (let i = 0; i < passwordFeatures.length; i++) {
-        password += getPasswordChar();
-    }
+  let password = "";
+  for (let i = 0; i < passwordLength; i++) {
+    password += getPasswordChar();
+  }
 
-    resultEl.innerHTML = password;
+  resultEl.innerHTML = password;
 });
