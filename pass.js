d8361e51d6c91ee1573ba4b3c70601b939311504
kissosDev <kissosDev>
2022-06-21 22:55:27 +0300

    Arrange the code

--- pass.js
+++ pass.js
@@ -1,5 +1,10 @@
-const log = console.log;
-console.log(`Liram Put Resek`);
+let passwordLength = 20;
+const passwordFeatures = {
+  includeUpper: true,
+  includeLower: true,
+  includeNumber: true,
+  includeSymbol: true,
+};
 
 function getRandomArbitrary(min, max) {
   return Math.random() * (max - min) + min;
@@ -9,13 +14,6 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-let asciPassPossibilities = [];
-let asciiRanges = [
-  [65, 90],
-  [97, 122],
-  [48, 57],
-  [33, 47],
-];
 function addPosibilitiesToArray() {
   let rangeItem = 0;
   let element = asciiRanges[rangeItem];
@@ -36,7 +34,6 @@ function addPosibilitiesToArray() {
     }
   }
 }
-
 function getPasswordChar() {
   let passChar = String.fromCharCode(
     asciPassPossibilities[getRandomInt(asciPassPossibilities.length)]
@@ -44,89 +41,17 @@ function getPasswordChar() {
   return passChar;
 }
 
-let passwordLength = 20;
 function initiateAsciPassPossibilitiesArray() {
   asciPassPossibilities = [];
 }
 
-// function getFullPassword() {
-//     for (let i = 0; i < passwordFeatures.length; i++) {
-//         let password = password += getPasswordChar();
-//     }
-//     return password;
-// }
-
-// function getLowerCaseChar() {
-//     let lowerChar = String.fromCharCode(getRandomArbitrary(97, 122));
-//     return lowerChar;
-// }
-
-// function getUpperCaseChar() {
-//     let upperChar = String.fromCharCode(getRandomArbitrary(65, 90));
-//     return upperChar;
-// }
-
-// function getNumberChar() {
-//     let numChar = String.fromCharCode(getRandomArbitrary(48, 57));
-//     return numChar;
-// }
-
-// function getSymbolChar() {
-//     let symbolChar = String.fromCharCode(getRandomArbitrary(33, 47));
-//     return symbolChar;
-// }
-
-// function getPassword() {
-//     let password = "";
-//     for (let i = 0; i < passwordFeatures.length; i++) {
-
-//         if (passwordFeatures.includeLower) {
-//             password += getLowerCaseChar();
-//         }
-//         if (passwordFeatures.includeUpper) {
-//             password += getUpperCaseChar();
-//         }
-//         if (passwordFeatures.includeNumber) {
-//             password += getNumberChar();
-//         }
-//         if (passwordFeatures.includeSymbol) {
-//             password += getSymbolChar();
-//         }
-//     }
-//     return password;
-// }
-
-const passwordFeatures = {
-  // length: 20,
-  includeUpper: true,
-  includeLower: true,
-  includeNumber: true,
-  includeSymbol: true,
-};
-
-/*
-cost symbols = [];
-const numbers = [];
-const upperCase = [];
-const lowerCase = [];
-
-const initialPass = getMinimumRequirements();
-const possibleChars = getPossibleCharSubset();
-const rawPassword = initialPass + (length(password) - length(initial) number of random choices from possibleChars)
-const password = shuffle(rawPassword)
-
-
-*/
-
-// function generatePassword(passwordFeatures) {}
-
-const resultEl = document.getElementById("result");
-const lengthEl = document.getElementById("length");
-const uppercaseEl = document.getElementById("uppercase");
-const lowercaseEl = document.getElementById("lowercase");
-const numbersEl = document.getElementById("numbers");
-const symbolsEl = document.getElementById("symbols");
-const generateEl = document.getElementById("generate");
+let asciPassPossibilities = [];
+let asciiRanges = [
+  [65, 90],
+  [97, 122],
+  [48, 57],
+  [33, 47],
+];
 
 function generatePasswordFeatures(passwordFeatures) {
   passwordLength = lengthEl.value;
@@ -135,6 +60,14 @@ function generatePasswordFeatures(passwordFeatures) {
   passwordFeatures.includeNumber = numbersEl.checked;
   passwordFeatures.includeSymbol = symbolsEl.checked;
 }
+
+const resultEl = document.getElementById("result");
+const lengthEl = document.getElementById("length");
+const uppercaseEl = document.getElementById("uppercase");
+const lowercaseEl = document.getElementById("lowercase");
+const numbersEl = document.getElementById("numbers");
+const symbolsEl = document.getElementById("symbols");
+const generateEl = document.getElementById("generate");
 
 lowercaseEl.addEventListener("click", function updateLowerCase(e) {
   passwordFeatures.includeLower = lowercaseEl.checked;
